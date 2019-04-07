8138a0b8aee2f687661cb57754606b154e0bee47
Dirk Breuer <mail@breuer.io>
2019-04-07 10:07:33 +0200

    Some refactoring to mode selection

--- rechenmeister.rb
+++ rechenmeister.rb
@@ -3,6 +3,92 @@
 class Rechenmeister
   CHEAT_MODE = "tzx"
 
+  module Mode
+    attr_reader :name, :ranges
+
+    def initialize(name:, ranges:)
+      @name = name
+      @op = op
+      @ranges = ranges
+    end
+
+    def operands(round)
+      range = select_range(round)
+
+      solvable_order_for_operands(range.sample, range.sample)
+    end
+
+    def solvable_order_for_operands(first, second)
+      [first, second]
+    end
+
+    def select_range(round)
+      case round
+      when 0..4
+        ranges[0]
+      when 5..7
+        ranges[1]
+      when (7..)
+        ranges[2]
+      end
+    end
+  end
+
+  class Add
+    include Mode
+
+    def op
+      :+
+    end
+
+    def solvable_order_for_operands(first, second)
+      return [first - 10, second].shuffle if first > 10 && second > 10
+
+      super
+    end
+  end
+
+  class Substract
+    include Mode
+
+    def op
+      :-
+    end
+
+    def operands(round)
+      range = select_range(round)
+
+      solvable_order_for_operands(range[0].sample, range[1].sample)
+    end
+
+    def solvable_order_for_operands(first, second)
+      return [second, first] if first < second
+
+      super
+    end
+  end
+
+  class Multiply
+    include Mode
+
+    def op
+      :*
+
+    end
+  end
+
+  class Divide
+    include Mode
+
+    def op
+      :/
+    end
+
+    def solvable_order_for_operands(first, second)
+      [first * second, [first, second].sample]
+    end
+  end
+
   OP_CHAR = {
     "*": "×",
     "+": "+",
@@ -13,6 +99,37 @@ class Rechenmeister
   def initialize
     @current_round = 0
     @score = 0
+
+    @all_modes = {
+      '1' => Add.new(
+        name: "Addieren",
+        ranges: [
+          (3...12).to_a,
+          (12...23).to_a,
+          (15...25).to_a,
+        ]),
+      '2' => Substract.new(
+        name: "Substrahieren",
+        ranges: [
+          [(3...14).to_a, (3...14).to_a],
+          [(3...23).to_a, (3...23).to_a],
+          [(3...20).to_a, (20...70).to_a]
+        ]),
+      '$' => Multiply.new(
+        name: "Multiplizieren",
+        ranges: [
+          (2...5).to_a,
+          (3...7).to_a,
+          (5...10).to_a,
+        ]),
+      "!" => Divide.new(
+        name: "Dividieren",
+        ranges: [
+          (2...5).to_a,
+          (3...7).to_a,
+          (5...10).to_a,
+        ])
+    }
 
     intro
   end
@@ -51,42 +168,8 @@ class Rechenmeister
     puts "!) Dividieren"
     puts
     print "Wähle 1, 2, $ oder !: "
-    @mode = gets.strip
 
-    case @mode
-    when '1'
-      @mode_name = "Addieren"
-      @op = :+
-      @range = [
-        (3...12).to_a,
-        (12...23).to_a,
-        (15...25).to_a,
-      ]
-    when "2"
-      @mode_name = "Substrahieren"
-      @op = :-
-      @range = [
-        (3...14).to_a,
-        (3...20).to_a,
-        (3...30).to_a,
-      ]
-    when "$"
-      @mode_name = "Multiplizieren"
-      @op = :*
-      @range = [
-        (2...5).to_a,
-        (3...7).to_a,
-        (5...10).to_a,
-      ]
-    when "!"
-      @mode_name = "Dividieren"
-      @op = :/
-      @range = [
-        (2...5).to_a,
-        (3...7).to_a,
-        (5...10).to_a,
-      ]
-    else
+    unless @mode = @all_modes[gets.strip]
       puts
       puts "Mmmh, das kenn ich gar nicht, nimm doch lieber was anders ;)"
       puts
@@ -95,7 +178,7 @@ class Rechenmeister
     end
 
     puts
-    puts "OK, dann rechnen wir #{@mode_name}!"
+    puts "OK, dann rechnen wir #{@mode.name}!"
     puts
   end
 
@@ -119,39 +202,17 @@ class Rechenmeister
   def finish
     duration = Time.now - @start_time
 
-    threshold = @max_rounds * 1.1
+    threshold = @max_rounds * 1
 
     final_score = [
       ((threshold / (duration/60)) * 15).round,
       50
     ].min
 
-    puts "Toll #{@name}, hast #{final_score} Spielminuten gewonnen!"
+    system "ponysay --pony rainbowsalute 'Toll #{@name}, du hast #{final_score} Spielminuten gewonnen!'"
+    puts
 
     exit
-  end
-
-  def first_and_second_number
-    range = if @current_round > 7
-      @range[2]
-    elsif @current_round > 4
-      @range[1]
-    else
-      @range[0]
-    end
-
-    first = range.sample
-    second = range.sample
-
-    return [second, first] if @op == :- && first < second
-
-    return [first - 10, second].shuffle if @op == :+ && first > 10 && second > 10
-
-    if @op == :/
-      return [first * second, [first, second].sample]
-    end
-
-    [first, second]
   end
 
   def start
@@ -159,18 +220,18 @@ class Rechenmeister
 
     finish if @current_round >= @max_rounds
 
-    first, second = first_and_second_number
+    first, second = @mode.operands(@current_round)
 
-    print "Deine Aufgabe ist: #{first} #{OP_CHAR[@op]} #{second} = "
+    print "Deine Aufgabe ist: #{first} #{OP_CHAR[@mode.op]} #{second} = "
     while result = gets.strip
       if @cheat_mode
         result = eval(result)
       end
 
-      if result.to_i == first.send(@op, second)
+      if result.to_i == first.send(@mode.op, second)
         break
       else
-        print "Das war leider falsch. Probier es doch nochmal: #{first} #{OP_CHAR[@op]} #{second} = "
+        print "Das war leider falsch. Probier es doch nochmal: #{first} #{OP_CHAR[@mode.op]} #{second} = "
       end
     end
     puts "#{result} ist richtig. Weiter so!"
